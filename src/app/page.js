db9935cc7b022617e9b5ebaf33fcce1eae2dd74e
chandan-zaag <chandan@zaagsys.com>
2024-03-10 11:17:11 +0600

    feat: creates home folders

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,24 @@
 import Layouts from "@/layouts";
+import ChooseUsContainer from "@/sections/Home/ChooseUs/ChooseUsContainer";
 import AboutUs from "@/sections/Home/Hero/AboutUs";
 import HeroContainer from "@/sections/Home/Hero/HeroContainer";
+import MainCategoriesContainer from "@/sections/Home/MainCategories/MainCategoriesContainer";
+import MarketingContainer from "@/sections/Home/MarketingSection/MarketingContainer";
+import OfferContainer from "@/sections/Home/Offer/OfferContainer";
+import PioneeringExcellenceContainer from "@/sections/Home/PioneeringExcellence/PioneeringExcellenceContainer";
+import ProductContainer from "@/sections/Home/Product/ProductContainer";
 import Image from "next/image";
 
 export default function Home() {
     return (
         <Layouts.Primary>
             <HeroContainer />
+            <ProductContainer />
+            <MainCategoriesContainer />
+            <OfferContainer />
+            <MarketingContainer />
+            <ChooseUsContainer />
+            <PioneeringExcellenceContainer />
         </Layouts.Primary>
     );
 }
---
+++ src/sections/Home/PioneeringExcellence/PioneeringExcellenceContainer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PioneeringExcellenceContainer = () => {
+    return <div>PioneeringExcellenceContainer</div>;
+};
+
+export default PioneeringExcellenceContainer;
