c22dd10725dade2247967dd4bc237b8ef3ef7f52
Abdullah al noman <noman@zaagsys.com>
2024-03-10 19:56:07 +0600

    add : marketing section

--- src/sections/Home/MarketingSection/MarketingContainer.jsx
+++ src/sections/Home/MarketingSection/MarketingContainer.jsx
@@ -1,62 +1,17 @@
 import React from "react";
+import SingleMarketingSection from "./SingleMarketingSection";
 
 const MarketingContainer = () => {
     return (
         <div className="bg-[#F5F5F5]">
-            <div className="container mx-auto px-6 sm:px-3 lg:py-20 sm:py-5 py-4 space-y-10 ">
+            <div className="container mx-auto px-6 sm:px-3  lg:py-20 sm:py-5 py-9 space-y-10 ">
                 {marketingSectionData.map((data, index) => {
-                    // console.log(data.videoLink);
                     return (
-                        <div
-                            className={`flex gap-6 justify-between ${
-                                index === 1
-                                    ? "flex-col lg:flex-row-reverse"
-                                    : "flex-col lg:flex-row"
-                            } `}
+                        <SingleMarketingSection
                             key={index}
-                        >
-                            <div className="flex-1 ">
-                                <div
-                                    className={`flex flex-col gap-4 w-[100%] lg:w-[80%] text-left ${
-                                        index === 1 ? "ml-auto" : ""
-                                    }`}
-                                >
-                                    <h2 className="text-[24px] lg:text-[48px] font-[600]">
-                                        {data.title}
-                                    </h2>
-                                    <div className="flex flex-col gap-4">
-                                        {data.details.map((data, indx) => (
-                                            <p
-                                                key={indx}
-                                                className="text-[16px] lg:text-[20px]"
-                                            >
-                                                {data}
-                                            </p>
-                                        ))}
-                                    </div>
-                                    <button className="px-6 py-[14px] bg-[#0B2848] text-white rounded-[25px] w-full md:w-[283px] text-xs md:text-[16px]">
-                                        EXPLORE OUR STRAINS
-                                    </button>
-                                </div>
-                            </div>
-                            {/* <div className=" w-[587px] h-[650px]  rounded-[25px] relative">
-                                <video
-                                    src={data.videoLink}
-                                    autoPlay
-                                    muted
-                                    loop
-                                    // className="w-[587px] h-[760px] rounded-[25px] "
-                                    width="587"
-                                    height="650"
-                                />
-
-                                <div className="absolute w-[90%]  bottom-1 left-[5%] sm:left-[5%] right-[50%] inline-block mb-[-20px]  sm:mb-[-20px]  md:mb-[-30px] lg:mb-[-30px] border bg-slate-400 px-5 py-5 rounded-[25px]  bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-30 border-gray-400">
-                                    <h3 className="text-center text-[12px] lg:text-[24px] text-white">
-                                        {data.videoDetails}
-                                    </h3>
-                                </div>
-                            </div> */}
-                        </div>
+                            data={data}
+                            index={index}
+                        />
                     );
                 })}
             </div>
---
+++ src/sections/Home/MarketingSection/SingleMarketingSection.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+function SingleMarketingSection({ data, index }) {
+    return (
+        <div
+            className={`flex gap-9 justify-between ${
+                index === 1
+                    ? "flex-col lg:flex-row-reverse items-center"
+                    : "flex-col lg:flex-row items-center"
+            } `}
+        >
+            <div className="flex-1">
+                <div
+                    className={`flex flex-col gap-4 w-[100%] xl:w-[80%] text-left ${
+                        index === 1 ? "ml-auto" : ""
+                    }`}
+                >
+                    <h2 className="text-xl lg:text-5xl md:text-4xl sm:text-3xl font-[600]">
+                        {data.title}
+                    </h2>
+                    <div className="flex flex-col gap-4">
+                        {data.details.map((detail, indx) => (
+                            <p
+                                key={indx}
+                                className="text-[16px] lg:text-[20px]"
+                            >
+                                {detail}
+                            </p>
+                        ))}
+                    </div>
+                    <button className="px-6 py-[14px] bg-[#0B2848] text-white rounded-[25px] w-full md:w-[283px] text-xs md:text-[16px]">
+                        EXPLORE OUR STRAINS
+                    </button>
+                </div>
+            </div>
+            <div className="flex-1 rounded-[25px] relative">
+                <video
+                    src={data.videoLink}
+                    autoPlay
+                    muted
+                    loop
+                    className=" xl:h-[650px] lg:h-[550px] md:h-[470px] sm:h-[400px] h-[350px] 2xl:w-[650px] lg:w-auto sm:w-[500px] rounded-[25px] object-fill"
+                />
+
+                <div className="absolute w-[90%] 2xl:w-[78%] bottom-1 left-[5%] sm:left-[5%] right-[50%] inline-block mb-[-20px]  sm:mb-[-20px]  md:mb-[-30px] lg:mb-[-30px] border bg-slate-400 px-5 py-5 rounded-[25px]  bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-30 border-gray-400">
+                    <h3 className="text-center text-[12px] lg:text-2xl md:text-xl sm:text-base  text-white">
+                        {data.videoDetails}
+                    </h3>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default SingleMarketingSection;
