9f8fb03fa9d43b48526bb844841da8c01a0fe776
meem-zaag <s.raihan@zaagsys.com>
2024-03-11 15:54:25 +0600

    removed unnecessary files

--- src/sections/Home/Filter/index.js
+++ src/sections/Home/Filter/index.js
@@ -1,16 +1,8 @@
 import Category from "./Category";
-import SortBy from "./SortBy";
-import Availability from "./Availability";
-import Ratings from "./Ratings";
-import Colors from "./Colors";
 import Price from "./Price";
 
 const FilterOptions = {
     Category,
-    SortBy,
-    Availability,
-    Ratings,
-    Colors,
     Price,
 };
 
