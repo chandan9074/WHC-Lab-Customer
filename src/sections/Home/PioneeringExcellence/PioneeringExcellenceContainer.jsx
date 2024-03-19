acddde8b1ee15124fb5517ed7301df92e1deabf2
meem-zaag <s.raihan@zaagsys.com>
2024-03-19 10:30:23 +0600

    implemented title for pioneering section

--- src/sections/Home/PioneeringExcellence/PioneeringExcellenceContainer.jsx
+++ src/sections/Home/PioneeringExcellence/PioneeringExcellenceContainer.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const PioneeringExcellenceContainer = () => {
-    return <div>PioneeringExcellenceContainer</div>;
+    return (
+        <div className="container mx-auto px-6 py-9 md:py-[120px]">
+            <div className="space-y-9">
+                <h1 className="text-2xl font-semibold leading-9 md:text-[32px] md:leading-12 text-brand-blue-800 max-w-[900px]">
+                    Pioneering Excellence in Brewing, Distilling, and Lab
+                    Analysis Innovations
+                </h1>
+            </div>
+        </div>
+    );
 };
 
 export default PioneeringExcellenceContainer;
