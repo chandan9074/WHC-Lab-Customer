e325ecc5a71bdc88732896ef7b8da43a0239317a
meem-zaag <s.raihan@zaagsys.com>
2024-03-31 11:33:57 +0600

    created wishlist page

---
+++ src/app/(protected)/profile/my-wishlist/page.jsx
@@ -0,0 +1,11 @@
+import Wishlists from "@/sections/Profile/Wishlist/Wishlists";
+
+const MyWishLists = async () => {
+    return (
+        <div className="py-5 px-2 md:p-12">
+            <Wishlists />
+        </div>
+    );
+};
+
+export default MyWishLists;
