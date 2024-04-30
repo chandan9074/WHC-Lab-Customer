d2853fd3c636629b88a9f6eae2ccbfe75594015b
Abdullah al noman <noman@zaagsys.com>
2024-04-30 10:37:31 +0600

    add : change email procedure.

--- src/app/(protected)/change-email-verification/page.jsx
+++ src/app/(protected)/change-email-verification/page.jsx
@@ -1,16 +1,52 @@
+"use client";
 import Loader from "@/components/common/Loader";
 import Layouts from "@/layouts";
 import VerificationForm from "@/sections/Common/VerificationForm";
-import React, { Suspense } from "react";
+import UserService from "@/services/UserService/UserService";
+import { Spin } from "antd";
+import { getCookie } from "cookies-next";
+import React, { Suspense, useState } from "react";
+import { toast } from "react-toastify";
+import { useRouter } from "next/navigation";
+import { MY_ACCOUNT_PATH } from "@/helpers/slug";
 
 const ChangePhone = () => {
+    const [loading, setLoading] = useState(false);
+    const token = getCookie("accessToken");
+    const userInfo = getCookie("userInfo");
+    const _userInfo = JSON.parse(userInfo);
+    const router = useRouter();
+
+    const handleUpdate = async (code) => {
+        try {
+            setLoading(true);
+            const data = {
+                action: "change_email",
+                otp: parseInt(code),
+                email: _userInfo.primaryEmail,
+            };
+            const response = await UserService.verifyOTP(data, token);
+
+            if (response?.status === 200) {
+                toast.success(response?.message);
+                router.push(MY_ACCOUNT_PATH);
+            }
+        } catch (e) {
+            toast.error(e?.message);
+        } finally {
+            setLoading(false);
+        }
+    };
+
     return (
         <Suspense fallback={<Loader />}>
             <Layouts.Primary>
+                <Spin fullscreen spinning={loading} />
                 <section className="container mx-auto py-6 px-4 flex justify-center">
                     <VerificationForm
                         title="email address"
                         verifyShortForm="asdasdasavc@gamil.com"
+                        handleUpdate={handleUpdate}
                     />
                 </section>
             </Layouts.Primary>
