3bf17ed168de91672459b87d937da71223078176
meem-zaag <s.raihan@zaagsys.com>
2024-05-27 16:13:07 +0600

    map in distributor page is loading the locations from api search based on selection

--- src/services/DistributorsService/index.js
+++ src/services/DistributorsService/index.js
@@ -1,15 +1,29 @@
-import { DISTRIBUTORS_URL } from "@/helpers/apiURLS";
+import { DISTINCT_CATEGORY_URL, DISTINCT_COUNTRY_URL, DISTRIBUTORS_URL } from "@/helpers/apiURLS";
 import MakeApiCall from "../MakeApiCall";
 
-async function getDistributors(){
-    const res = await MakeApiCall({apiUrl:DISTRIBUTORS_URL
+async function getDistributors(query){
+    const res = await MakeApiCall({apiUrl:DISTRIBUTORS_URL, query: query
+    });
+
+    return res;
+}
+
+async function getDistinctCountry(){
+    const res = await MakeApiCall({apiUrl:DISTINCT_COUNTRY_URL
+    });
+
+    return res;
+}
+
+async function getDistinctCategory(){
+    const res = await MakeApiCall({apiUrl:DISTINCT_CATEGORY_URL
     });
 
     return res;
 }
 
 const DistributorsService = {
-    getDistributors
+    getDistributors,getDistinctCountry,getDistinctCategory
 }
 
 export default DistributorsService;
