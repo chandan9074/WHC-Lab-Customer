2a984b05e55c3831dae39974c92f4dae7265143d
meem-zaag <s.raihan@zaagsys.com>
2024-06-03 12:20:02 +0600

    blog page's pagination fixed

--- src/sections/Blog/BlogContainer.jsx
+++ src/sections/Blog/BlogContainer.jsx
@@ -2,20 +2,34 @@
 import React, { Fragment, useState } from "react";
 import BlogInfoCard from "./BlogInfoCard";
 import { Pagination } from "antd";
+import { getCookie } from "cookies-next";
+import MakeApiCall from "@/services/MakeApiCall";
+import { BLOG_URL } from "@/helpers/apiURLS";
 
-function BlogContainer({ blogsData, blogDataTotalPage, blogDataLimit }) {
+function BlogContainer({ blogsData }) {
     const [current, setCurrent] = useState(1);
+    const [list, setList] = useState(blogsData?.docs || []);
+    const token = getCookie("accessToken");
+
+    const onChange = async (page) => {
+        console.log(page);
+        const res = await MakeApiCall({
+            apiUrl: BLOG_URL,
+            headers: { Authorization: token },
+            query: { page: page, limit: 9 },
+        });
+        setList(res.docs);
+
+        setCurrent(page);
+    };
     // const handlePagination = () => {
     //     console.log("page changed");
     // };
-    const onChange = (page) => {
-        console.log(page);
-        setCurrent(page);
-    };
+
     return (
         <>
             <div className="grid grid-col-1 md:grid-cols-3 gap-6">
-                {blogsData?.map((data) => (
+                {list?.map((data) => (
                     <Fragment key={data._id}>
                         <BlogInfoCard data={data} />
                     </Fragment>
@@ -25,8 +39,9 @@ function BlogContainer({ blogsData, blogDataTotalPage, blogDataLimit }) {
                 <Pagination
                     defaultCurrent={1}
                     current={current}
-                    total={blogDataTotalPage * blogDataLimit}
-                    onChange={(value) => setCurrent(value)}
+                    total={blogsData?.totalDocuments}
+                    pageSize={9}
+                    onChange={onChange}
                 />
             </div>
         </>
